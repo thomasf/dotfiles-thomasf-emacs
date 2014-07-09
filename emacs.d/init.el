d30423505a093db1a45c29c211d15f9755463813
Thomas Frössman <thomasf@jossystem.se>
2014-07-09 13:32:56 +0200

    add toggle-quotes and remove prev impl from init

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2061,42 +2061,6 @@ sTo this: ")
      ((equal 1 (point))
       (message "beginning of file reached, this was probably a mistake.")))))
 
-;;;; toggle-quotes
-(defun current-quotes-char ()
-  (nth 3 (syntax-ppss)))
-
-(defalias 'point-is-in-string-p 'current-quotes-char)
-
-(defun move-point-forward-out-of-string ()
-  (while (point-is-in-string-p) (forward-char)))
-
-(defun move-point-backward-out-of-string ()
-  (while (point-is-in-string-p) (backward-char)))
-
-(defun alternate-quotes-char ()
-  (if (eq ?' (current-quotes-char)) ?\" ?'))
-
-(defun toggle-quotes ()
-  (interactive)
-  (if (point-is-in-string-p)
-      (let ((old-quotes (char-to-string (current-quotes-char)))
-            (new-quotes (char-to-string (alternate-quotes-char)))
-            (start (make-marker))
-            (end (make-marker)))
-        (save-excursion
-          (move-point-forward-out-of-string)
-          (backward-delete-char 1)
-          (set-marker end (point))
-          (insert new-quotes)
-          (move-point-backward-out-of-string)
-          (delete-char 1)
-          (insert new-quotes)
-          (set-marker start (point))
-          (replace-string new-quotes (concat "\\" new-quotes) nil start end)
-          (replace-string (concat "\\" old-quotes) old-quotes nil start end)))
-    (error "Point isn't in a string")))
-
-(bind-key "C-'" 'toggle-quotes)
 ;;; functions: MISC
 ;;;; cycle ispell languages
 ;; Languages for spellinc cycling
@@ -3091,10 +3055,16 @@ for the current buffer's file name, and the line number at point."
 (use-package sqlite
   :ensure t
   :defer t)
+
 ;;;; string-inflection
 (use-package string-inflection
   :ensure t
   :commands string-inflection-cycle)
+
+(use-package toggle-quotes
+  :ensure t
+  :commands (toggle-quotes)
+  :bind ("C-'" . toggle-quotes))
 
 ;;;; unfill-paragraph
 (use-package unfill
