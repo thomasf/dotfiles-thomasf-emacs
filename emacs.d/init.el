b3e96cf164010d8da31e8f0f627501fa01f42da1
Thomas Frössman <thomasf@jossystem.se>
2014-07-31 17:28:25 +0200

    Disable ipython python config
    Due to errors, probably changes in python.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6808,16 +6808,17 @@ minibuffer."
   :init
   (progn
     (setq
-     python-shell-interpreter "ipython"
-     python-shell-interpreter-args ""
-     python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-     python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-     python-shell-completion-setup-code
-     "from IPython.core.completerlib import module_completion"
-     python-shell-completion-module-string-code
-     "';'.join(module_completion('''%s'''))\n"
-     python-shell-completion-string-code
-     "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+     ;; python-shell-interpreter "ipython"
+     ;; python-shell-interpreter-args ""
+     ;; python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+     ;; python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+     ;; python-shell-completion-setup-code
+     ;; "from IPython.core.completerlib import module_completion"
+     ;; python-shell-completion-module-string-code
+     ;; "';'.join(module_completion('''%s'''))\n"
+     ;; python-shell-completion-string-code
+     ;; "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"
+     )
 
     (defun my-python-mode-hook ()
       (setq-local idle-update-delay 2))
