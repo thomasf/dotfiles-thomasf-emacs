517c63afee99c491cb86582400fed73fe0261df9
Thomas Frössman <thomasf@jossystem.se>
2014-10-06 22:35:03 +0200

    Add empty makeup package to work around bug

---
+++ emacs.d/lisp/makeup.el
@@ -0,0 +1,4 @@
+;; just empty, because somehow ../makeup.el gets compiled into php-mode.el
+;; requireing a package called makeup as well, I do not have time to
+;; investigate atm. leave this stub instead.
+(provide 'makeup)
