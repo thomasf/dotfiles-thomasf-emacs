d69422b65ea72a16f51785ac04f1202f3fbc1690
Thomas Frössman <thomasf@jossystem.se>
2015-02-27 13:47:22 +0100

    Add support for /* global ... */ externs in js2-mode
    copied from https://github.com/magnars/.emacs.d/blob/master/settings/setup-js2-mode.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -7669,6 +7669,23 @@ super-method of this class, e.g. super(Classname, self).method(args)."
         (js2-imenu-extras-setup)))
 
     (font-lock-add-keywords
+    ;; After js2 has parsed a js file, we look for jslint globals decl comment ("/* global Fred, _, Harry */") and
+    ;; add any symbols to a buffer-local var of acceptable global vars
+    ;; Note that we also support the "symbol: true" way of specifying names via a hack (remove any ":true"
+    ;; to make it look like a plain decl, and any ':false' are left behind so they'll effectively be ignored as
+    ;; you can;t have a symbol called "someName:false"
+    (add-hook 'js2-post-parse-callbacks
+              (lambda ()
+                (when (> (buffer-size) 0)
+                  (let ((btext (replace-regexp-in-string
+                                ": *true" " "
+                                (replace-regexp-in-string "[\n\t ]+" " " (buffer-substring-no-properties 1 (buffer-size)) t t))))
+                    (mapc (apply-partially 'add-to-list 'js2-additional-externs)
+                          (split-string
+                           (if (string-match "/\\* *global *\\(.*?\\) *\\*/" btext) (match-string-no-properties 1 btext) "")
+                           " *, *" t))
+                    ))))
+
      'js2-mode `(("\\(function *\\)("
                   (0 (progn (compose-region
                              (match-beginning 1)
