8a03205b14c81b48537ebf0f2a032ee7b016bc3a
Thomas Frössman <thomasf@jossystem.se>
2015-03-06 13:45:29 +0100

    Comment out recentf merge hack
    - It does not allow for recentf-cleanup

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3838,43 +3838,46 @@ If FILE already exists, signal an error."
   (progn
     ;; the following code is from emacswiki for merging recentf of multiple
     ;; emacs instances.
-    (defun recentf-save-list ()
-      "Save the recent list.
-Load the list from the file specified by `recentf-save-file',
-merge the changes of your current session, and save it back to the
-file."
-      (interactive)
-      (let ((instance-list (copy-list recentf-list)))
-        (recentf-load-list)
-        (recentf-merge-with-default-list instance-list)
-        (recentf-write-list-to-file)))
 
-    (defun recentf-merge-with-default-list (other-list)
-      "Add all items from `other-list' to `recentf-list'."
-      (dolist (oitem other-list)
-        ;; add-to-list already checks for equal'ity
-        (add-to-list 'recentf-list oitem)))
+    ;;     (defun recentf-save-list ()
+    ;;       "Save the recent list.
+    ;; Load the list from the file specified by `recentf-save-file',
+    ;; merge the changes of your current session, and save it back to the
+    ;; file."
+    ;;       (interactive)
+    ;;       (let ((instance-list (copy-list recentf-list)))
+    ;;         (recentf-load-list)
+    ;;         (recentf-merge-with-default-list instance-list)
+    ;;         (recentf-write-list-to-file)))
 
-    (defun recentf-write-list-to-file ()
-      "Write the recent files list to file.
-Uses `recentf-list' as the list and `recentf-save-file' as the
-file to write to."
-      (condition-case error
-          (with-temp-buffer
-            (erase-buffer)
-            (set-buffer-file-coding-system recentf-save-file-coding-system)
-            (insert (format recentf-save-file-header (current-time-string)))
-            (recentf-dump-variable 'recentf-list recentf-max-saved-items)
-            (recentf-dump-variable 'recentf-filter-changer-current)
-            (insert "\n \n;;; Local Variables:\n"
-                    (format ";;; coding: %s\n" recentf-save-file-coding-system)
-                    ";;; End:\n")
-            (write-file (expand-file-name recentf-save-file))
-            (when recentf-save-file-modes
-              (set-file-modes recentf-save-file recentf-save-file-modes))
-            nil)
-        (error
-         (warn "recentf mode: %s" (error-message-string error)))))))
+    ;;     (defun recentf-merge-with-default-list (other-list)
+    ;;       "Add all items from `other-list' to `recentf-list'."
+    ;;       (dolist (oitem other-list)
+    ;;         ;; add-to-list already checks for equal'ity
+    ;;         (add-to-list 'recentf-list oitem)))
+
+    ;;     (defun recentf-write-list-to-file ()
+    ;;       "Write the recent files list to file.
+    ;; Uses `recentf-list' as the list and `recentf-save-file' as the
+    ;; file to write to."
+    ;;       (condition-case error
+    ;;           (with-temp-buffer
+    ;;             (erase-buffer)
+    ;;             (set-buffer-file-coding-system recentf-save-file-coding-system)
+    ;;             (insert (format recentf-save-file-header (current-time-string)))
+    ;;             (recentf-dump-variable 'recentf-list recentf-max-saved-items)
+    ;;             (recentf-dump-variable 'recentf-filter-changer-current)
+    ;;             (insert "\n \n;;; Local Variables:\n"
+    ;;                     (format ";;; coding: %s\n" recentf-save-file-coding-system)
+    ;;                     ";;; End:\n")
+    ;;             (write-file (expand-file-name recentf-save-file))
+    ;;             (when recentf-save-file-modes
+    ;;               (set-file-modes recentf-save-file recentf-save-file-modes))
+    ;;             nil)
+    ;;         (error
+    ;;          (warn "recentf mode: %s" (error-message-string error)))))
+
+    ))
 
 (use-package savehist
   :if (and
