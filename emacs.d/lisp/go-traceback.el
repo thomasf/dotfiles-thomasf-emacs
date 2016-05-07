d95ba9242d33874381e037f0d32c8c2f3f3af4e7
Thomas Frössman <thomasf@jossystem.se>
2016-05-07 21:28:14 +0200

    Minor changes to go-traceback

--- emacs.d/lisp/go-traceback.el
+++ emacs.d/lisp/go-traceback.el
@@ -4,32 +4,20 @@
 (defun go-traceback-refresh ()
   "Refresh traceback buffer"
   (interactive)
-  (go-traceback go-traceback-url))
+  (go-traceback go-traceback-buffer-url))
 
-
-;; (defconst my-js-mode-syntax-table
-;;   (let ((table (make-syntax-table)))
-;;     ;; ' is a string delimiter
-;;     (modify-syntax-entry ?' "\"" table)
-;;     ;; " is a string delimiter too
-;;     (modify-syntax-entry ?\" "\"" table)
-
-;;     ;; / is punctuation, but // is a comment starter
-;;     (modify-syntax-entry ?/ ". 12" table)
-;;     ;; \n is a comment ender
-;;     (modify-syntax-entry ?\n ">" table)
-;;     table))
+(defcustom go-traceback-url "http://localhost:8080/debug/pprof/goroutine?debug=2"
+  "The default url where pprof is expeted to be running at. ")
 
 (define-compilation-mode go-traceback-mode "go traceback mode"
-  ;; :syntax-table my-js-mode-syntax-table
   (font-lock-fontify-buffer)
   (setq-local compilation-error-regexp-alist
               '(("^[\t ]*\\[\\([^(].*\\):\\([1-9][0-9]*\\)\\(\\]\\)?:in " 2 3)))
-  (setq-local go-traceback-url nil)
+  (setq-local go-traceback-buffer-url nil)
   (define-key go-traceback-mode-map (kbd "g" )
     '(lambda ()
        (interactive)
-       (go-traceback go-traceback-url t (window-start)))))
+       (go-traceback go-traceback-buffer-url t (window-start)))))
 
 
 (defun go-traceback (&optional url no-move w-start)
@@ -38,7 +26,7 @@
   (lexical-let* ((no-move no-move)
                  (w-start w-start))
     (request
-     (or url "http://localhost:6067/debug/pprof/goroutine?debug=2")
+     (or url go-traceback-url)
      :type "GET"
      :parser (lambda ()(buffer-string))
      :success (function*
@@ -51,7 +39,7 @@
                        (insert data)
                        (goto-char (if no-move p-point (point-min)))
                        (go-traceback-mode)
-                       (setq go-traceback-url (request-response-url response))))
+                       (setq go-traceback-buffer-url (request-response-url response))))
 
                    (let ((w (display-buffer b)))
                      (when w-start
