d2d5cf3372c455079104ef559f7d885b617a519a
Thomas Frössman <thomasf@jossystem.se>
2016-09-01 13:06:13 +0200

    Disable recentf merge lists patch
    It might not work as expected anymore.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4275,57 +4275,62 @@ If FILE already exists, signal an error."
         (message "Aborting"))))
   :config
   (progn
-    (defvar recentfs-list-on-last-sync nil
-      "List of recent files reference point.")
+    ;;; TODO: this doesnt seem to work as expected anymore
+    ;;   (defvar recentfs-list-on-last-sync nil
+    ;;       "List of recent files reference point.")
 
-    (defun recentfs-update-sync ()
-      "Load saved projects from `recentf-list'."
-      (setq recentfs-list-on-last-sync
-            (and (sequencep recentf-list)
-               (copy-sequence recentf-list))))
+    ;;     (defun recentfs-update-sync ()
+    ;;       "Load saved projects from `recentf-list'."
+    ;;       (setq recentfs-list-on-last-sync
+    ;;             (and (sequencep recentf-list)
+    ;;                (copy-sequence recentf-list))))
 
-    (defadvice recentf-load-list (after recentfs-loaded-sync activate)
-      (recentfs-update-sync))
+    ;;     (defadvice recentf-load-list (after recentfs-loaded-sync activate)
+    ;;       (recentfs-update-sync))
 
-    (defadvice recentf-save-list (around recentfs activate)
-      (recentfs-merge-lists)
-      ad-do-it
-      (recentfs-update-sync))
+    ;;     (defadvice recentf-save-list (around recentfs activate)
+    ;;       (recentfs-merge-lists)
+    ;;       ad-do-it
+    ;;       (recentfs-update-sync))
 
-    (defun recentfs-load-list ()
-      "Load a previously saved recent list and return it as a value
-instead of setting it."
-      (let ((file (expand-file-name recentf-save-file))
-            (recentf-filter-changer-current nil) ;; ignored atm
-            (recentf-list nil))
-        (when (file-readable-p file)
-          (load-file file))
-        recentf-list))
+    ;;     (defun recentfs-load-list ()
+    ;;       "Load a previously saved recent list and return it as a value
+    ;; instead of setting it."
+    ;;       (let ((file (expand-file-name recentf-save-file))
+    ;;             (recentf-filter-changer-current nil) ;; ignored atm
+    ;;             (recentf-list nil))
+    ;;         (when (file-readable-p file)
+    ;;           (load-file file))
+    ;;         recentf-list))
 
-    (defun recentfs-merge-lists ()
-      "Merge any change from `recentf-list'.
+    ;;     (defun recentfs-merge-lists ()
+    ;;       "Merge any change from `recentf-list'.
 
-This enables multiple Emacs processes to make changes without
-overwriting each other's changes."
-      (let* ((known-now recentf-list)
-             (known-on-last-sync recentfs-list-on-last-sync)
-             (known-on-file (recentfs-load-list))
-             (removed-after-sync (-difference known-on-last-sync known-now))
-             (removed-in-other-process
-              (-difference known-on-last-sync known-on-file))
-             (new-in-other-process
-              (-difference
-               known-on-file
-               (-concat removed-after-sync removed-in-other-process known-now)))
-             (result (-distinct
-                      (-difference
-                       (-concat new-in-other-process known-now)
-                       (-concat removed-after-sync removed-in-other-process)))))
-        (setq recentf-list result)))
+    ;; This enables multiple Emacs processes to make changes without
+    ;; overwriting each other's changes."
+    ;;       (let* ((known-now recentf-list)
+    ;;              (known-on-last-sync recentfs-list-on-last-sync)
+    ;;              (known-on-file (recentfs-load-list))
+    ;;              (removed-after-sync (-difference known-on-last-sync known-now))
+    ;;              (removed-in-other-process
+    ;;               (-difference known-on-last-sync known-on-file))
+    ;;              (new-in-other-process
+    ;;               (-difference
+    ;;                known-on-file
+    ;;                (-concat removed-after-sync removed-in-other-process known-now)))
+    ;;              (result (-distinct
+    ;;                       (-difference
+    ;;                        (-concat new-in-other-process known-now)
+    ;;                        (-concat removed-after-sync removed-in-other-process)))))
+    ;;         (setq recentf-list result)))
 
-    (cl-letf (((symbol-function 'message)
-               (lambda (fmt &rest _))))
-      (recentf-mode 1))))
+    ;;     (cl-letf (((symbol-function 'message)
+    ;;                (lambda (fmt &rest _))))
+    ;;       (recentf-mode 1))
+
+    )
+
+  )
 
 (use-package savehist
   :if (and (not noninteractive) (not degrade-p-minimalism))
@@ -5410,6 +5415,9 @@ See URL `https://github.com/golang/lint'."
   :mode "\\.go\\'"
   :config
   (progn
+    (use-package go-impl
+      :ensure t
+      :commands go-impl)
     (use-package go-guru
       :ensure t
       :commands (go-guru-hl-identifier-mode
@@ -5426,7 +5434,6 @@ See URL `https://github.com/golang/lint'."
       (progn
         (add-hook 'go-mode-hook #'go-guru-hl-identifier-mode)
         (use-package go-mode
-          :defer
           :config
           (progn
             (bind-key "M-." 'go-guru-definition go-mode-map)))))
