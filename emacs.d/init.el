215be7e967403b73b55dfb988f6f12216d0b2c01
Thomas Frössman <thomasf@jossystem.se>
2018-01-22 23:51:14 +0100

    Fix org babel config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4920,7 +4920,7 @@ Argument FILENAME File to insert."
      'org-babel-load-languages
      '((perl . t)
        (ruby . t)
-       (sh . t)
+       (shell . t)
        (python . t)
        (emacs-lisp . t)
        (dot . t)
