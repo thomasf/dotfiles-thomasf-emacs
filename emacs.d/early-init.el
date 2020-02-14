9e376272820fc4c03c1090e2ad800c8ecfad58b8
Thomas Frössman <thomasf@jossystem.se>
2020-02-14 21:09:19 +0100

    ..

--- emacs.d/early-init.el
+++ emacs.d/early-init.el
@@ -3,3 +3,9 @@
 (setq tool-bar-mode nil
       menu-bar-mode nil
       scroll-bar-mode nil)
+
+(setq frame-inhibit-implied-resize t
+      default-frame-alist '((vertical-scroll-bars . nil)
+                            (tool-bar-lines . 0)
+                            (menu-bar-lines . 0)
+                            (fullscreen . nil)))
