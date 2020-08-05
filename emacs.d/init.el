7644051a18a502f723ee077373f1f70da020450c
Thomas Frössman <thomasf@jossystem.se>
2020-08-05 23:27:57 +0200

    quick modeline-format

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -627,7 +627,26 @@ re-downloaded in order to locate PACKAGE."
               indicate-empty-lines nil
               transient-mark-mode t
               fill-column 79
-              fringes-outside-margins t)
+              fringes-outside-margins t
+              mode-line-format (list
+                                "%e"
+                                'mode-line-front-space
+                                'mode-line-mule-info
+                                ;; 'mode-line-client
+                                'mode-line-modified
+                                'mode-line-remote
+                                " "
+                                'mode-line-buffer-identification
+                                "  "
+                                'mode-line-position
+                                'smartrep-mode-line-string
+                                '(vc-mode vc-mode)
+                                "  "
+                                'mode-line-modes
+                                ;; 'mode-line-misc-info
+                                'mode-line-end-spaces
+                                )
+              )
 
 
 ;;;;; coding.c
